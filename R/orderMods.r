91011c8533791286b0b4b6fb361a74b7f734d8c2
Adam Sales <asales@wpi.edu>
2022-06-30 17:31:18 -0400

    updating orderMods.r

--- R/orderMods.r
+++ R/orderMods.r
@@ -13,7 +13,17 @@ flpsDat<-do.call("rbind",flpsDat)
 for(df in 2:10){
   print(df)
   assign(paste0('ordMod',df),
-         glmer(firstTry~ns(PartOrder,df)+(ns(PartOrder,df)|ProblemSet)+(1|StuID),family=binomial,data=flpsDat,subset=Z==1)
+         glmer(firstTry~ns(pOrd,df)+(ns(pOrd,df)|ProblemSet)+(1|StuID),family=binomial,data=flpsDat,subset=Z==1)
          )
   save(list=paste0('ordMod',df),file=paste0('fittedModels/ordMod',df,'.RData'))
 }
+
+pd=flpsDat%>%distinct(ProblemSet,pOrd)%>%mutate(StuID=0)%>%arrange(pOrd)
+for(df in 2:10){
+    mmm=paste0('ordMod',df)
+    if(exists(mmm)){
+        mod=get(mmm )
+        pd[[paste0('pred',df)]]<-predict(mod,pd,allow.new.levels=TRUE)
+    }}
+
+ggplot(pd,aes(pOrd,pred4))+geom_line()+facet_wrap(~ProblemSet)
