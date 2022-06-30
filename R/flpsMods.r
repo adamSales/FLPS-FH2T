0069fb75491edec482685e63db799697abb11753
Adam Sales <asales@wpi.edu>
2022-06-30 17:40:33 -0400

    fitting some flps models

---
+++ R/flpsMods.r
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(rstan)
+library(splines)
+
+options(mc.cores = 8)
+rstan_options(auto_write = TRUE)
+
+load('data/probPartDat.RData')
+load('data/studDat.RData')
+load('data/flpsDat.RData')
+
+### first try flps
+studDat1=studDat%>%
+  filter(StuID%in%flpsDat$StuID,!is.na(Scale.Score7))%>%
+  arrange(StuID)%>%
+  mutate(
+    stud=as.numeric(as.factor(StuID))
+  )
+
+### flps dat only IDs that are in studDat1 & new stud id
+flpsDat1=right_join(flpsDat,select(studDat1,StuID,stud))
+
+sdat=with(flpsDat1,
+          list(
+            studentM=stud,
+            prob=as.numeric(as.factor(probPart)),
+            firstTry=firstTry))
+sdat$X=model.matrix(
+  ~MALE+race+as.factor(teach)+accelerated+
+    Scale.Score5+pre.total_math_score+EIP+ESOL+GIFTED+
+    IEP+AbsentDays6+pre.avg_time_on_tasks+
+    pre_MA_total_score+pre_MSE_total_score+
+    pre_PS_tasks_total_score,
+  data=studDat1)
+sdat$X[,-1]=scale(sdat$X[,-1])
+sdat$Z=ifelse(studDat1$rdm_condition=='ASSISTments',1,0)
+sdat$Y=studDat1$Scale.Score7
+
+sdat$nprobWorked=nrow(flpsDat1)
+sdat$ncov=ncol(sdat$X)
+sdat$nstud=nrow(studDat1)
+sdat$nprob=max(sdat$prob)
+
+flpsRasch1=stan('R/rasch1lev.stan',data=sdat,chains=8,iter=5000,warmup=4000,thin=2)
+save(flpsRasch1,sdat,file='fittedModels/flpsRasch1.RData')
